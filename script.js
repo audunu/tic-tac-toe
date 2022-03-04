154c3320199e87792b1dd004098c8bff2a083bbd
audunu <audunu@gmail.com>
2022-03-04 13:27:53 +0100

    make each player able to play

--- script.js
+++ script.js
@@ -1,33 +1,65 @@
 // player factory function
-const createPlayer = (name, marker) => {
-    return { name, marker }
-}
-
-
-
-// gameboard object
-const gameBoard = (() => {
-    let board = ['X', 'O', 'X', 'O', 'O', 'O', 'X', 'O', 'X'];
-
-    const render = () => {
-        const gridContainer = document.querySelector('.grid-container');
-        gameBoard.board.forEach(element => {
-            const newDiv = document.createElement('div');
-            newDiv.textContent = element;
-            newDiv.classList.add('square');
-            newDiv.addEventListener('click', (e) => {
-                
-            })
-            gridContainer.appendChild(newDiv);
-        })
-        
-    }
+const Player = (name, marker, isActive) => {
 
 
     return {
-        board,
-        render,
+        name,
+        marker,
+        isActive,
     };
+}
+
+// gameboard object
+const gameBoard = (() => {
+    let board = ['', '', '', '', '', '', '', '', ''];
+
+    return {
+        board,
+
+    };
+})();
+
+
+const game = (() => {
+
+    const player1 = Player('player1', 'X', true);
+    const player2 = Player('player2', 'O', false);
+
+
+    const render = () => {
+        const gridContainer = document.querySelector('.grid-container');
+        gameBoard.board.forEach((element, index) => {
+            const newDiv = document.createElement('div');
+            newDiv.textContent = element;
+            newDiv.classList.add('square');
+            newDiv.setAttribute('data-index', index);
+            newDiv.addEventListener('click', (e) => {
+                e.preventDefault();
+                if (player1.isActive) {
+                    gameBoard.board[index] = player1.marker;
+                    player1.isActive = false;
+                    player2.isActive = true;
+                }
+                else if (player2.isActive) {
+                    gameBoard.board[index] = player2.marker;
+                    player2.isActive = false;
+                    player1.isActive = true;
+                    
+                }
+
+                gridContainer.innerHTML = '';
+                render();
+            })
+            gridContainer.appendChild(newDiv);
+        })
+
+    }
+    return {
+        render,
+    }
+
+
+
 })();
 
 
@@ -36,8 +68,5 @@ const gameBoard = (() => {
 
 
 
-
-
-
-gameBoard.render();
+game.render();
 
