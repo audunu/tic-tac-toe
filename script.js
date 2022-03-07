a4c6277771d228ed514c8ec2d933f0d41f346240
audunu <audunu@gmail.com>
2022-03-07 11:40:08 +0100

    add restart button

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 // player factory function
 
 
+const player1Input = document.querySelector('#player1').value;
+const player2Input = document.querySelector('#player2').value;
+
+const startBtn = document.querySelector('.pop-up button');
+
+const popUp = document.querySelector('.pop-up');
+
+startBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    popUp.style.visibility = 'hidden';
+    game.render();
+});
+
+const restartBtn = document.querySelector('.restartBtn-container button');
+restartBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    gameBoard.reset();
+    game.render();
+
+})
 
 
 const Player = (name, marker, isActivePlayer) => {
-
 
     return {
         name,
@@ -26,9 +45,15 @@ const gameBoard = (() => {
         [2, 5, 8],
         [0, 4, 8],
         [2, 4, 6],
-      ];
+    ];
+
+    const reset = () => {
+        for (let i = 0; i < board.length; i++)
+            board[i] = '';
+    }
 
     return {
+        reset,
         board,
         winningCombos,
     };
@@ -37,10 +62,8 @@ const gameBoard = (() => {
 
 const game = (() => {
 
-    
-
-    const player1 = Player('player1', 'X', true);
-    const player2 = Player('player2', 'O', false);
+    const player1 = Player('Player 1', 'X', true);
+    const player2 = Player('Player 2', 'O', false);
     let gameOver = false;
 
     const checkWinner = () => {
@@ -50,24 +73,21 @@ const game = (() => {
             let c = gameBoard.board[gameBoard.winningCombos[i][2]];
             if (a === b && b == c && a !== '') {
                 gameOver = true;
-                a === 'X' ? document.querySelector('.text').textContent = 'Congrats to Player 1!!!' : document.querySelector('.text').textContent = 'Congrats to Player 2!!!';
+                a === 'X' ? document.querySelector('.text').textContent = `Congrats to ${player1.name}!!!` : document.querySelector('.text').textContent = `Congrats to ${player2.name}!!!`;
                 document.querySelector('.text').classList.add('green');
             }
         }
         if (gameOver === false && !gameBoard.board.includes('')) {
             document.querySelector('.text').textContent = 'It´s a tie!';
         }
-        
+
     }
-
-
-
-
-
 
     const render = () => {
         const gridContainer = document.querySelector('.grid-container');
+        gridContainer.innerHTML = '';
         gameBoard.board.forEach((element, index) => {
+            console.log('newsquare');
             const newDiv = document.createElement('div');
             newDiv.textContent = element;
             newDiv.classList.add('square');
@@ -79,13 +99,15 @@ const game = (() => {
                         gameBoard.board[index] = player1.marker;
                         player1.isActivePlayer = false;
                         player2.isActivePlayer = true;
-                        document.querySelector('.text').textContent = 'Player 2 to play';
+                        document.querySelector('.text').textContent = `${player2.name} to play`;
+
                     }
                     else if (player2.isActivePlayer) {
                         gameBoard.board[index] = player2.marker;
                         player2.isActivePlayer = false;
                         player1.isActivePlayer = true;
-                        document.querySelector('.text').textContent = 'Player 1 to play';
+                        document.querySelector('.text').textContent = `${player1.name} to play`;
+
 
                     }
                     checkWinner();
@@ -107,11 +129,11 @@ const game = (() => {
 })();
 
 
-
-
-
-
-
 game.render();
+
+
+
+
+
 
 
